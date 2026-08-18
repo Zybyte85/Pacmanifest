feaf29bc90660b89d918186203477a76fc7fec6a
Caden Stephens <cade.step01@gmail.com>
2026-08-18 08:22:53 -0600

    feat: add installing older version of package from Arch Linux Archive

--- cmd/sync.go
+++ cmd/sync.go
@@ -5,9 +5,13 @@ package cmd
 
 import (
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"regexp"
+	"runtime"
 	"strings"
 
 	"github.com/Zybyte85/mypm/internal/config"
@@ -44,6 +48,7 @@ func init() {
 func installFromManifest() {
 	var pacmanPkgs []string
 	var aurPkgs []string
+	var archivePkgs []manifest.Package
 
 	manifestPath := filepath.Join(config.GetConfigPath(), "manifest.pkgs")
 	pkgs, err := manifest.ParseManifest(manifestPath)
@@ -52,27 +57,56 @@ func installFromManifest() {
 		return
 	}
 	for _, pkg := range pkgs {
+		var latestVersion string
+
+		if pkg.Version != "latest" {
+			latestVersion, err = seeLatestVersionOfPackage(pkg.Name)
+
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+		}
+
 		if pkg.IsAUR {
 			// If the package is an AUR package, use the AUR helper to install
 			aurPkgs = append(aurPkgs, pkg.Name)
 
-		} else if pkg.Version == "latest" {
+		} else if pkg.Version == "latest" || pkg.Version == latestVersion {
 			// Not AUR, and latest version, so install normally
 			pacmanPkgs = append(pacmanPkgs, pkg.Name)
+		} else {
+			// Not AUR, and specific version, so install from archive
+			archivePkgs = append(archivePkgs, pkg)
 		}
 	}
-	commandInstall(pacmanPkgs, "pacman", true)
-	commandInstall(aurPkgs, aurHelper, false)
+	runInstallCommand(pacmanPkgs, "pacman", []string{"-S", "--noconfirm", "--needed"}, true)
+	runInstallCommand(aurPkgs, aurHelper, []string{"-S", "--noconfirm", "--needed"}, false)
+	archiveInstall(archivePkgs)
 }
 
-func commandInstall(pkgs []string, command string, sudo bool) {
+func seeLatestVersionOfPackage(pkgName string) (string, error) {
+	cmd := exec.Command("pacman", "-Si", pkgName)
+	output, err := cmd.Output()
+	if err != nil {
+		return "", fmt.Errorf("Failed to get latest package version: %v", err)
+	}
+
+	lines := strings.Split(string(output), "\n")
+	_, versionStr, _ := strings.Cut(lines[2], ": ")
+	// Split the version string to remove the release number
+	latestVersion, _, _ := strings.Cut(versionStr, "-")
+
+	return string(latestVersion), nil
+}
+
+func runInstallCommand(pkgs []string, command string, args []string, sudo bool) {
 	if len(pkgs) == 0 {
 		// Nothing to install
 		return
 	}
 
 	// Build the argument list for the package manager
-	args := []string{"-S", "--noconfirm", "--needed"}
 	args = append(args, pkgs...)
 
 	var cmd *exec.Cmd
@@ -90,4 +124,107 @@ func commandInstall(pkgs []string, command string, sudo bool) {
 	if err := cmd.Run(); err != nil {
 		fmt.Println(err)
 	}
+}
+
+func archiveInstall(pkgs []manifest.Package) error {
+	if len(pkgs) == 0 {
+		// Nothing to install
+		return fmt.Errorf("No packages to install")
+	}
+
+	var architectureStr string
+	switch runtime.GOARCH {
+	case "amd64":
+		architectureStr = "x86_64"
+	case "arm64":
+		architectureStr = "aarch64"
+	default:
+		architectureStr = "x86_64" // Fallback to x86_64
+	}
+
+	var filePaths []string
+	cachePath := filepath.Join(config.GetConfigPath(), "cache")
+
+	for _, pkg := range pkgs {
+		// Fetch archive list to find package release number
+		fmt.Printf("Downloading %s from archive\n", pkg.Name)
+		baseUrl := fmt.Sprintf("https://archive.archlinux.org/packages/%s/%s", pkg.Name[:1], pkg.Name)
+
+		resp, err := http.Get(baseUrl)
+		if err != nil {
+			return fmt.Errorf("Error fetching directory for %s: %v", pkg.Name, err)
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode != http.StatusOK {
+			return fmt.Errorf("Package $s not found on archive (Status: %d)", pkg.Name, resp.Status)
+		}
+
+		bodyBytes, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return fmt.Errorf("Error reading response body for %s: %v", pkg.Name, err)
+		}
+		htmlContent := string(bodyBytes)
+
+		pattern := fmt.Sprintf(`%s-%s-([a-zA-Z0-9.]+)-(?:%s|any)\.pkg\.tar\.zst`,
+			regexp.QuoteMeta(pkg.Name),
+			regexp.QuoteMeta(pkg.Version),
+			architectureStr,
+		)
+		re := regexp.MustCompile(pattern)
+		matches := re.FindStringSubmatch(htmlContent)
+
+		if len(matches) < 2 {
+			fmt.Printf("Could not find a matching archive file for %s version %s\n", pkg.Name, pkg.Version)
+			continue
+		}
+
+		// pkgrel := matches[1]
+		exactFileName := matches[0]
+
+		filePath := filepath.Join(cachePath, exactFileName)
+		filePaths = append(filePaths, filePath)
+
+		downloadUrl := fmt.Sprintf("%s/%s", baseUrl, exactFileName)
+
+		err = os.MkdirAll(cachePath, 0o755)
+		if err != nil {
+			return fmt.Errorf("Error creating cache directory: %v", err)
+		}
+
+		downloadFile(downloadUrl, filePath)
+	}
+
+	runInstallCommand(filePaths, "pacman", []string{"-U", "--noconfirm", "--needed"}, true)
+
+	for _, filePath := range filePaths {
+		os.Remove(filePath)
+	}
+
+	return nil
+}
+
+func downloadFile(url, dest string) error {
+	out, err := os.Create(dest)
+	if err != nil {
+		return fmt.Errorf("Error creating file: %v", err)
+	}
+	defer out.Close()
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return fmt.Errorf("Error downloading file: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("Failed to download file (Status: %d)", resp.StatusCode)
+	}
+
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		return fmt.Errorf("Error saving file: %v", err)
+	}
+
+	return nil
 }
